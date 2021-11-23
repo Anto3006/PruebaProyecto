933a6b5fd90d01dd61598dac31aaf1300fe11d3a
Antonio Piedra <anto.piedrapach@hotmail.com>
2021-11-23 14:33:09 -0600

    AA

--- Planetario-PI-IS-BD/Controllers/HomeController.cs
+++ Planetario-PI-IS-BD/Controllers/HomeController.cs
@@ -17,5 +17,17 @@ namespace Planetario.Controllers {
       return View();
     }
 
+    public ActionResult About() {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact() {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+
   }
 }
